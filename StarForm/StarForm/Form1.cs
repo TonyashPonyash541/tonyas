3747996495d818601aafc75899f815c9a0145475
Татьяна <167198763+TonyashPonyash541@users.noreply.github.com>
2024-09-26 17:47:43 +0900

    Add files via upload

---
+++ StarForm/StarForm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StarForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.panel1.Paint += new PaintEventHandler(Panel1_Paint);
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Fuchsia, 2);
+            SolidBrush brush = new SolidBrush(Color.Fuchsia);
+
+            Point[] starPoints = {
+                new Point(100, 10),
+                new Point(120, 70),
+                new Point(190, 70),
+                new Point(130, 110),
+                new Point(150, 180),
+                new Point(100, 140),
+                new Point(50, 180),
+                new Point(70, 110),
+                new Point(10, 70),
+                new Point(80, 70)
+            };
+
+            g.FillPolygon(brush, starPoints);
+            g.DrawPolygon(pen, starPoints);
+        }
+    }
+}
---
+++ StarForm/StarForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("StarForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StarForm")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a8762625-42dd-4daa-8a9d-6bbbb9f0be82")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
