5a38116c32ba3122146ce6ca8e6220b0c33bf2fc
Татьяна <167198763+TonyashPonyash541@users.noreply.github.com>
2024-10-04 20:32:35 +0900

    Add files via upload

---
+++ Random Number Generator/Random Number Generator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Number_Generator
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int min) && int.TryParse(textBox2.Text, out int max))
+            {
+                if (min > max)
+                {
+                    MessageBox.Show("Минимальное значение не может быть больше максимального.");
+                }
+                else
+                {
+                    int randomNumber = random.Next(min, max + 1);
+                    label1.Text = $"Случайное число: {randomNumber}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа.");
+            }
+        }
+    }
+}
---
+++ Random Number Generator/Random Number Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Random Number Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Random Number Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bc37539a-c9bb-4da1-806d-ace88dc051f5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
